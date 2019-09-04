82db55211553f0afa497d146a2b692878ecd5f07
blashadow <blackzerogamer@gmail.com>
2019-09-04 00:46:00 -0400

    Avoid swiftlint to check pods libraries

---
+++ Pelis/Data/Network/MoviesServiceClient.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesServiceClient.swift
+//  Pelis
+//
+//  Created by Blashadow on 9/4/19.
+//  Copyright © 2019 Blashadow. All rights reserved.
+//
+
+import UIKit
+
+class MoviesServiceClient: NSObject {
+
+}
